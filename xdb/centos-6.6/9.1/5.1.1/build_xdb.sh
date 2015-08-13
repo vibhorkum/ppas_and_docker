ca9fa7766e0ddac0894fb38fc1e1dd739d6dd1b2
Richard Yen <richyen3@gmail.com>
2015-08-13 15:24:49 -0700

    minor tweaks removing pgbench_history, as it doesn't play nicely with replication--could make it work, but don't have the time for it. Add automatic conflict resolution export IPs for other scripts create pub database before starting edb-xdbpubserver

--- xdb/centos-6.6/9.1/5.1.1/build_xdb.sh
+++ xdb/centos-6.6/9.1/5.1.1/build_xdb.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-MDN_IP=`hostname -i`
-LIST_OF_OTHER_MASTER_IPS=''
+PUBSVR_IP=`hostname -i`
+
+# Make these IPs available for other scripts
+export MDN_IP=`hostname -i`
+export OTHER_MASTER_IPS=''
 
 # Start xDB
+createdb -h ${PUBSVR_IP} xdb_pub
 service edb-xdbpubserver start
 
 # Load data into MDN
 pgbench -h ${MDN_IP} -i -F 10 -s 10 edb
+# psql -h ${MDN_IP} -c "ALTER TABLE pgbench_history add primary key (tid,bid,aid,delta,mtime)" edb
 
 # Build xDB replication infrastructure
 java -jar /usr/ppas-xdb-5.1/bin/edb-repcli.jar -repsvrfile /usr/ppas-xdb-5.1/etc/xdb_repsvrfile.conf -uptime
 java -jar /usr/ppas-xdb-5.1/bin/edb-repcli.jar -addpubdb -repsvrfile /usr/ppas-xdb-5.1/etc/xdb_repsvrfile.conf -dbtype enterprisedb -dbhost ${MDN_IP} -dbuser enterprisedb -dbpassword `cat /usr/ppas-xdb-5.1/etc/xdb_repsvrfile.conf | grep pass | cut -f2- -d'='` -database edb -repgrouptype m -nodepriority 1 -dbport 5432
-java -jar /usr/ppas-xdb-5.1/bin/edb-repcli.jar -createpub xdbtest -repsvrfile /usr/ppas-xdb-5.1/etc/xdb_repsvrfile.conf -pubdbid 1 -reptype T -tables public.pgbench_accounts public.pgbench_branches public.pgbench_history public.pgbench_tellers -repgrouptype M
+java -jar /usr/ppas-xdb-5.1/bin/edb-repcli.jar -createpub xdbtest -repsvrfile /usr/ppas-xdb-5.1/etc/xdb_repsvrfile.conf -pubdbid 1 -reptype T -tables public.pgbench_accounts public.pgbench_branches public.pgbench_tellers -repgrouptype M -standbyconflictresolution 1:E 2:E 3:E
 
 # Add other masters
-for i in ${LIST_OF_OTHER_MASTER_IPS}
+for i in ${OTHER_MASTER_IPS}
 do
     java -jar /usr/ppas-xdb-5.1/bin/edb-repcli.jar -repsvrfile /usr/ppas-xdb-5.1/etc/xdb_repsvrfile.conf -addpubdb -dbtype enterprisedb -dbhost ${i} -dbuser enterprisedb -dbpassword `cat /usr/ppas-xdb-5.1/etc/xdb_repsvrfile.conf | grep pass | cut -f2- -d'='` -database edb -repgrouptype m -dbport 5432 -initialsnapshot -replicatepubschema true
 done
