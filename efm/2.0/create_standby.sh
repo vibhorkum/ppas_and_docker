1280b023d6427bc233d0a7fe1f7fd5b93a9a4882
Richard Yen <richyen3@gmail.com>
2015-08-27 15:31:20 -0700

    renamed to set_as_standby.sh

--- efm/2.0/create_standby.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Script to be run on to-be standby
-MASTER_HOST=''
-
-service ppas-9.4 initdb
-pg_basebackup -U repuser -h ${MASTER_HOST} -D /var/lib/ppas/9.4/data -xP
-service ppas-9.4 start
