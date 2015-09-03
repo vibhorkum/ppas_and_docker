7ff0db09be1a433e1f995a7db42b6f251d1a41ed
Richard Yen <richyen3@gmail.com>
2015-09-03 07:17:37 -0700

    Use get.enterprisedb.com Don't use internal site; allows us not to require VPN to build

--- pem/5.0/install_pem_server.sh
+++ pem/5.0/install_pem_server.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# install sslutils
-cd /root/sslutils-1.1
-make USE_PGXS=1 
-make USE_PGXS=1 install
-
 # create placeholder dirs
 mkdir -p /opt/apache-php
 mkdir -p /opt/php_edbpem
 
 # extract Apache/PHP package
-/root/pem_server-5.0.0-2-linux-x64.run --extract-php_edbpem /opt/php_edbpem --extract-apache-php /opt/apache-php
+/root/pem_server-5.0.2-2-linux-x64.run --extract-php_edbpem /opt/php_edbpem --extract-apache-php /opt/apache-php
 
 # install Apache/PHP
-/opt/apache-php/apachephp-2.4.10-5.5.19-1-linux-x64.run --mode unattended --optionfile /root/apache_php_install_optionfile
-/opt/php_edbpem/php_edbpem-5.5.19-5.0.0-2-linux-x64.run --mode unattended
+APACHEPHP_INSTALL=`ls /opt/apache-php | grep apachephp-.*-linux-x64.run`
+PHP_EDBPEM_INSTALL=`ls /opt/php_edbpem | grep php_edbpem-.*-linux-x64.run`
+/opt/apache-php/${APACHEPHP_INSTALL} --mode unattended --optionfile /root/apache_php_install_optionfile
+/opt/php_edbpem/${PHP_EDBPEM_INSTALL} --mode unattended
 
 # install PEM Server
-/root/pem_server-5.0.0-2-linux-x64.run --mode unattended --optionfile /root/pem_install_optionfile
+/root/pem_server-5.0.2-2-linux-x64.run --mode unattended --optionfile /root/pem_install_optionfile
