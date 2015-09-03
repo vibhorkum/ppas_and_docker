96606cde979691214b581b710199556289d62871
Richard Yen <richyen3@gmail.com>
2015-09-03 09:19:58 -0700

    Don't use hard-coded filename

--- pem/5.0/install_pem_server.sh
+++ pem/5.0/install_pem_server.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+PEM_SERVER_INSTALL=`ls /root | grep pem_server-.*-linux-x64.run`
+
 # create placeholder dirs
 mkdir -p /opt/apache-php
 mkdir -p /opt/php_edbpem
 
 # extract Apache/PHP package
-/root/pem_server-5.0.2-2-linux-x64.run --extract-php_edbpem /opt/php_edbpem --extract-apache-php /opt/apache-php
+/root/${PEM_SERVER_INSTALL} --extract-php_edbpem /opt/php_edbpem --extract-apache-php /opt/apache-php
 
 # install Apache/PHP
 APACHEPHP_INSTALL=`ls /opt/apache-php | grep apachephp-.*-linux-x64.run`
@@ -14,4 +16,4 @@ PHP_EDBPEM_INSTALL=`ls /opt/php_edbpem | grep php_edbpem-.*-linux-x64.run`
 /opt/php_edbpem/${PHP_EDBPEM_INSTALL} --mode unattended
 
 # install PEM Server
-/root/pem_server-5.0.2-2-linux-x64.run --mode unattended --optionfile /root/pem_install_optionfile
+/root/${PEM_SERVER_INSTALL} --mode unattended --optionfile /root/pem_install_optionfile
