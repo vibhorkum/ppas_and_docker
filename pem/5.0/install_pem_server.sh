d7d003661d1dc48e5e1f9d4b11e6c0264de5a627
Richard Yen <richyen3@gmail.com>
2015-09-14 17:04:06 -0700

    Print status to screen for better UX

--- pem/5.0/install_pem_server.sh
+++ pem/5.0/install_pem_server.sh
@@ -2,7 +2,11 @@
 
 PEM_SERVER_INSTALL=`ls /root | grep pem_server-.*-linux-x64.run`
 
+# setting temporary password
+psql edb enterprisedb -c "ALTER USER enterprisedb WITH PASSWORD 'abc123'" &> /dev/null
+
 # create placeholder dirs
+printf "\e[0;33m==== Extracting dependencies for PEM Server ====\n\e[0m"
 mkdir -p /opt/apache-php
 mkdir -p /opt/php_edbpem
 
@@ -10,10 +14,12 @@ mkdir -p /opt/php_edbpem
 /root/${PEM_SERVER_INSTALL} --extract-php_edbpem /opt/php_edbpem --extract-apache-php /opt/apache-php
 
 # install Apache/PHP
+printf "\e[0;33m==== Installing Apache/PHP for Web-based PEM Console ====\n\e[0m"
 APACHEPHP_INSTALL=`ls /opt/apache-php | grep apachephp-.*-linux-x64.run`
 PHP_EDBPEM_INSTALL=`ls /opt/php_edbpem | grep php_edbpem-.*-linux-x64.run`
 /opt/apache-php/${APACHEPHP_INSTALL} --mode unattended --prefix /usr/edb-apache-php
 /opt/php_edbpem/${PHP_EDBPEM_INSTALL} --mode unattended
 
 # install PEM Server
+printf "\e[0;33m==== Installing PEM Server ====\n\e[0m"
 /root/${PEM_SERVER_INSTALL} --mode unattended --optionfile /root/pem_install_optionfile
--- pem/pem_demo.sh
+++ pem/pem_demo.sh
@@ -6,6 +6,7 @@ EDB_PASSWORD=''
 
 if [[ ${1} == 'destroy' ]]
 then
+	printf "\e[0;31m==== Destroying existing PEM cluster ====\n\e[0m"
   for i in server agent1 agent2
   do
     docker rm -f pem-${i}
@@ -15,17 +16,18 @@ fi
 
 if [[ "x${EDB_LOGIN}" == "x" || "x${EDB_PASSWORD}" == "x" ]]
 then
-  echo "Please fill in your EDB login and password in ${0}"
+  printf "\e[0;31mPlease fill in your EDB login and password in ${0}[0m"
 	exit 1
 fi
 
 if [[ ${1} == 'image' ]]
 then
   # Warn user before building image--give them a chance to stop and go set the passwords
-  echo "Building Image -- be sure to set passwords as necessary (sleeping for 10sec so you can abort)"
+  printf "\e[0;31mBuilding Image -- be sure to set passwords as necessary (sleeping for 10sec so you can abort)\e[0m"
   sleep 10
 
   # Create Image
+	printf "\e[0;33m==== Building new image for PEM cluster ====\n\e[0m"
   PWD=`pwd`
   cd ${PWD}/5.0
   docker build -t "${IMAGE_NAME}" .
@@ -33,6 +35,7 @@ then
 fi
 
 # Create Containers
+printf "\e[0;33m==== Building containers for PEM cluster ====\n\e[0m"
 for i in server agent1 agent2
 do
   C_NAME="pem-${i}"
@@ -40,11 +43,14 @@ do
 done
 
 # set up PEM Server
+printf "\e[0;33m>>> SETTING UP PEM SERVER\n\e[0m"
 docker exec -t pem-server sed -i "s/existing-user.*/existing-user=${EDB_LOGIN}/" /root/pem_install_optionfile
 docker exec -t pem-server sed -i "s/existing-password.*/existing-password=${EDB_PASSWORD}/" /root/pem_install_optionfile
 docker exec -t pem-server bash --login -c "/root/install_pem_server.sh"
+docker exec -t pem-server bash --login -c "service pemagent start && tail -f /dev/null" &
 
 # set up PEM Agents
+printf "\e[0;33m>>> SETTING UP PEM AGENTS\n\e[0m"
 for i in 1 2
 do
   MASTER_IP=`docker exec -it pem-server ifconfig | grep Bcast | awk '{ print $2 }' | cut -f2 -d':' | xargs echo -n`
@@ -54,6 +60,7 @@ done
 
 if [[ `uname` = 'Darwin' ]]
 then
+  printf "\e[0;33m>>> LAUNCHING PEM CONSOLE\n\e[0m"
   boot2dockerip=`boot2docker ip`;
   port=`docker ps -f name=pem-server | grep 8443 | sed -e 's/.*0.0.0.0:\(.*\)->8443.*/\1/'`
   open https://${boot2dockerip}:${port}/pem
