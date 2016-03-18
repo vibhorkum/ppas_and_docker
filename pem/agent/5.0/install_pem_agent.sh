577a79a1e0223aab0f8689793ed5bc95b68820d8
Richard Yen <richyen3@gmail.com>
2016-03-17 17:28:32 -0700

    Clean up build output Redirect to /dev/null

--- pem/agent/5.0/install_pem_agent.sh
+++ pem/agent/5.0/install_pem_agent.sh
@@ -12,6 +12,6 @@ AGENT_ID=`hostname -i | cut -f4 -d '.'`
 
 /tmp/`ls /tmp/ | grep agent | grep run` --mode unattended --pghost ${PEM_SERVER_IP} --pguser enterprisedb --pgpassword abc123 --agent_description ${CONTAINER_NAME} --prefix /usr/pem-5.0
 
-find /usr/pem-5.0/agent/lib -type f | xargs -I% ln -s % /lib64/
+find /usr/pem-5.0/agent/lib -type f | xargs -I% ln -s % /lib64/ &> /dev/null
 
 service pemagent start
