e1c854aaf49f9a65be12b0d28bed402cb244aa82
Richard Yen <richyen3@gmail.com>
2017-01-06 16:55:01 -0800

    Retrofitting for PEM6

--- pem/pem_demo.sh
+++ pem/pem_demo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PEM_VER=6
 NUM_AGENTS=2
 
 if [[ ${1} == 'destroy' ]]
@@ -16,15 +17,21 @@ fi
 # Create Containers
 printf "\e[0;33m==== Building containers for PEM cluster ====\n\e[0m"
 printf "\e[0;33m>>> SETTING UP PEM SERVER\n\e[0m"
-  docker run --privileged=true --publish-all=true --interactive=false --tty=true -v /Users/${USER}/Desktop:/Desktop --hostname=${C_NAME} --detach=true --name=pem-server pem6_server:latest
-printf "\e[0;33m>>> SETTING UP PEM AGENTS\n\e[0m"
+docker run --privileged=true --publish-all=true --interactive=false --tty=true -v /Users/${USER}/Desktop:/Desktop --hostname=pem-server --detach=true --name=pem-server pem${PEM_VER}_server:latest
 
+printf "\e[0;33m>>> SETTING UP PEM AGENTS\n\e[0m"
 MASTER_IP=`docker exec -it pem-server ifconfig | grep Bcast | awk '{ print $2 }' | cut -f2 -d':' | xargs echo -n`
 for ((i=1;i<=${NUM_AGENTS};i++))
 do
   C_NAME="pem-agent${i}"
-  docker run --privileged=true --publish-all=true --interactive=false --tty=true -v /Users/${USER}/Desktop:/Desktop --hostname=${C_NAME} --detach=true --name=${C_NAME} pem6_agent:latest
-  docker exec -t pem-agent${i} bash --login -c "/tmp/install_pem_agent.sh ${MASTER_IP}"
+  docker run --privileged=true --publish-all=true --interactive=false --tty=true -v /Users/${USER}/Desktop:/Desktop --hostname=${C_NAME} --detach=true --name=${C_NAME} pem${PEM_VER}_agent:latest
+  if [[ ${PEM_VER} -eq 6 ]]
+  then
+    docker exec -t pem-agent${i} sed -i "s/%%PEM_SERVER_IP%%/${MASTER_IP}/" /tmp/register_pem_agent.sh
+    docker exec -t pem-agent${i} sed -i "s/%%AGENT_NAME%%/pemagent${i}/" /tmp/register_pem_agent.sh
+  else
+    docker exec -t pem-agent${i} bash --login -c "/tmp/install_pem_agent.sh ${MASTER_IP}"
+  fi
 done
 
 if [[ `uname` = 'Darwin' ]]
