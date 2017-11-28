9edd4233cd2525d6943fb74a78d5eef28d795dc8
Richard Yen <richyen3@gmail.com>
2017-11-28 15:49:16 -0800

    EDBAS 9.6 and 10 Dockerfiles Updated scripts for new install procedures

--- epas/build_from_installer.sh
+++ epas/build_from_installer.sh
@@ -15,8 +15,13 @@ fi
 INSTALLER_FILENAME=${1}
 VERSION=`echo ${INSTALLER_FILENAME} | cut -f2 -d'-'`
 PGMAJOR=`echo ${VERSION} | cut -f1-2 -d'.'`
+if [[ `echo ${1} | grep -c "edb-as"` -ge 1 ]]
+then
+  VERSION=`echo ${INSTALLER_FILENAME} | cut -f4 -d'-'`
+  PGMAJOR=`echo ${VERSION} | cut -f1 -d'.'`
+fi
+
 V=`echo ${PGMAJOR} | sed "s/\.//"`
 
 cat Dockerfile.installer_template | sed "s/%%PGMAJOR%%/${PGMAJOR}/" > Dockerfile
-
 docker build --build-arg INSTALLER_FILENAME=${INSTALLER_FILENAME} -t ppas${V}:${VERSION} . 2>&1 | tee ppas${V}_${VERSION}_build.log
