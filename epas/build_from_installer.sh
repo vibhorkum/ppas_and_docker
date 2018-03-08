1a1aaeffb40249f20196f600c9f2e0e4accfe540
Richard Yen <richyen3@gmail.com>
2018-03-07 23:35:04 -0800

    More intelligence

--- epas/build_from_installer.sh
+++ epas/build_from_installer.sh
@@ -12,16 +12,23 @@ then
   exit 1
 fi
 
+BASE_NAME="ppas"
 INSTALLER_FILENAME=${1}
 VERSION=`echo ${INSTALLER_FILENAME} | cut -f2 -d'-'`
 PGMAJOR=`echo ${VERSION} | cut -f1-2 -d'.'`
 if [[ `echo ${1} | grep -c "edb-as"` -ge 1 ]]
 then
+  BASE_NAME="epas"
   VERSION=`echo ${INSTALLER_FILENAME} | cut -f4 -d'-'`
   PGMAJOR=`echo ${VERSION} | cut -f1 -d'.'`
+  if [ $PGMAJOR -eq 9 ]
+  then
+    PGMAJOR=`echo ${VERSION} | cut -f1-2 -d'.'`
+  fi
 fi
 
 V=`echo ${PGMAJOR} | sed "s/\.//"`
 
 cat Dockerfile.installer_template | sed "s/%%PGMAJOR%%/${PGMAJOR}/" > Dockerfile
-docker build --build-arg INSTALLER_FILENAME=${INSTALLER_FILENAME} -t ppas${V}:${VERSION} . 2>&1 | tee ppas${V}_${VERSION}_build.log
+
+docker build --no-cache --build-arg INSTALLER_FILENAME=${INSTALLER_FILENAME} -t ${BASE_NAME}${V}:${VERSION} . 2>&1 | tee ppas${V}_${VERSION}_build.log
